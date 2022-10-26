0242447043c1286e400e24ce4d7b7e37b763003a
mohammad azim <mohammad@mohammad-azim-pc.scoiumtech.local>
2022-10-26 17:53:03 +0300

    add: Rout to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Home></Home>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </>
   );
 }
