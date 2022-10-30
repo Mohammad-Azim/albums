07bd80d23ad70abe744506ad344384b558ed0213
Mohammad-Azim <mohammad.azim@optimumpartners.co>
2022-10-30 12:15:35 +0200

    refactor: the Header make MobileMenu components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react"; // import state
 import { Link } from "react-router-dom";
+import MobileMenu from "./MobileMenu/MobileMenu";
 
 import "./Header.css";
 function Header(props) {
@@ -16,58 +17,13 @@ function Header(props) {
               <div className="tracking-wider ">PHOTOGRAPHY</div>
             </div>
           </div>
-          {/*  MOBILE-MENU */}
-          <section className=" flex lg:hidden justify-end ">
-            <div
-              className="HAMBURGER-ICON space-y-2"
-              onClick={() => setIsNavOpen((prev) => !prev)} // toggle isNavOpen state on click
-            >
-              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-            </div>
 
-            <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
-              {" "}
-              <div
-                className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
-                onClick={() => setIsNavOpen(false)} // change isNavOpen state to false to close the menu
-              >
-                <svg
-                  className="h-8 w-8 text-gray-600"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <line x1="18" y1="6" x2="6" y2="18" />
-                  <line x1="6" y1="6" x2="18" y2="18" />
-                </svg>
-              </div>
-              <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
-                <li className="border-b border-gray-400 my-8 uppercase">
-                  <a href="/about">Pregnancy</a>
-                </li>
-                <li className="border-b border-gray-400 my-8 uppercase">
-                  <a href="/portfolio">Babies</a>
-                </li>
-                <li className="border-b border-gray-400 my-8 uppercase">
-                  <a href="/contact">Family</a>
-                </li>
-                <li className="border-b border-gray-400 my-8 uppercase">
-                  <a href="/contact">Bio</a>
-                </li>
-                <li className="border-b border-gray-400 my-8 uppercase">
-                  <a href="/contact">Book Session</a>
-                </li>
-                <li className="border-b border-gray-400 my-8 uppercase">
-                  {!userName ? <Link to="/login">Log In</Link> : userName}
-                </li>
-              </ul>
-            </div>
-          </section>
+          {/*  MOBILE-MENU */}
+          <MobileMenu
+            setIsNavOpen={setIsNavOpen}
+            isNavOpen={isNavOpen}
+            userName={userName}
+          />
 
           <div className="justify-around italic font-hairline font-serif hidden lg:flex">
             <div>Pregnancy</div>
---
+++ src/components/Header/MobileMenu/MobileMenu.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function MobileMenu(props) {
+  return (
+    <section className=" flex lg:hidden justify-end ">
+      <div
+        className="HAMBURGER-ICON space-y-2"
+        onClick={() => props.setIsNavOpen((prev) => !prev)} // toggle isNavOpen state on click
+      >
+        <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+        <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+        <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+      </div>
+
+      <div className={props.isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+        {" "}
+        <div
+          className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
+          onClick={() => props.setIsNavOpen(false)} // change isNavOpen state to false to close the menu
+        >
+          <svg
+            className="h-8 w-8 text-gray-600"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <line x1="18" y1="6" x2="6" y2="18" />
+            <line x1="6" y1="6" x2="18" y2="18" />
+          </svg>
+        </div>
+        <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
+          <li className="border-b border-gray-400 my-8 uppercase">
+            <a href="/about">Pregnancy</a>
+          </li>
+          <li className="border-b border-gray-400 my-8 uppercase">
+            <a href="/portfolio">Babies</a>
+          </li>
+          <li className="border-b border-gray-400 my-8 uppercase">
+            <a href="/contact">Family</a>
+          </li>
+          <li className="border-b border-gray-400 my-8 uppercase">
+            <a href="/contact">Bio</a>
+          </li>
+          <li className="border-b border-gray-400 my-8 uppercase">
+            <a href="/contact">Book Session</a>
+          </li>
+          <li className="border-b border-gray-400 my-8 uppercase">
+            {!props.userName ? <Link to="/login">Log In</Link> : props.userName}
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
